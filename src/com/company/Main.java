dad94a53c80a124a6d44e8fbf312a7a9714f5ce7
pesu12 <sundberg_p@yahoo.com>
2018-02-27 23:24:01 +0100

    Adding Code for Lambds and java.time

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,58 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello7");
+        Main tester = new Main();
+
+        //with type declaration
+        MathOperation addition = (int a, int b) -> a + b;
+
+        //with out type declaration
+        MathOperation subtraction = (a, b) -> a - b;
+
+        //with return statement along with curly braces
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        //without return statement and without curly braces
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        //without parenthesis
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        //with parenthesis
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Mahesh");
+        greetService2.sayMessage("Suresh");
+
+        // Current date
+        LocalDate localDate = LocalDate.now();
+        System.out.println(localDate);
+
+        //Specific date
+        LocalDate localDate2 = LocalDate.of(2017, Month.MAY, 15);
+        System.out.println(localDate2);
+    }
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation) {
+        return mathOperation.operation(a, b);
     }
 }
